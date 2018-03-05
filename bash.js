4ddf7baa3e868b387f84c9050c3cd70416f5e301
GSSchneider <35821280+GSSchneider@users.noreply.github.com>
2018-03-05 12:06:39 -0500

    Clean up files; keep command functionality only in their respective functions in command.js; everything else in bash.js

--- bash.js
+++ bash.js
@@ -1,31 +1,12 @@
-// console.log(process);
+const commands = require('./commands');
 
-// process.stdout.write('prompt > ');
+let cmd;
 
-// // The stdin 'data' event fires after a user types in a line
-// process.stdin.on('data', function (data) {
-//   var cmd = data.toString().trim(); // remove the newline
-//   var date = Date(Date.now());
-// //   var 
-
-//   if(cmd === 'pwd') process.stdout.write(`${process.cwd()}`);
-//   if(cmd === 'date') process.stdout.write(date);
-//   process.stdout.write('\nprompt > ');
-  
-  //Mon Mar  5 10:58:04 EST 2018
-//   process.stdout.write('You typed: ' + cmd);
-//   process.stdout.write('\nprompt > ');
-
-// });
-console.log("Hello!")
 process.stdout.write('\nprompt > ');
-var cmd;
-process.stdin.on('data', function (data) {
-    cmd = data.toString().trim(); 
-    console.log(cmd)
-})
 
-var commands = require('./commands');
-// var userCommand =  'date' || 'pwd';
-commands['pwd']();
-// process.stdout.write('\nprompt > ')
+process.stdin.on('data', function (data) {
+    cmd = data.toString().trim();
+    commands[cmd]();
+    process.stdout.write('\nprompt > ');
+
+});
--- commands.js
+++ commands.js
@@ -1,21 +1,11 @@
-
 module.exports = {
     pwd: function() {
-      // pwd code
-      // process.stdin.on('data', function (data) {
-        // var cmd = data.toString().trim(); // remove the newline
-        // if(cmd === 'pwd') 
         process.stdout.write(`${process.cwd()}`);
-        process.stdout.write('\nprompt > ');
-        },
-    // },
+      },
+
     date: function() {
-      // var d = Date(Date.now());
-      // process.stdin.on('data', function (data) {
-        // var cmd = data.toString().trim();
-        if(cmd === 'date') process.stdout.write(d);
-        process.stdout.write('\nprompt > ');
-        // })
-    }
+      var d = Date(Date.now());
+        process.stdout.write(d);
+      }
 }
 
