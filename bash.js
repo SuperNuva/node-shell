65a3c06c4a4e3f5b9a8d48f6895247e52b5257d3
GSSchneider <35821280+GSSchneider@users.noreply.github.com>
2018-03-05 12:54:36 -0500

    Implement echo (w/o delimiter); account for args passed in

--- bash.js
+++ bash.js
@@ -6,7 +6,17 @@ process.stdout.write('\nprompt > ');
 
 process.stdin.on('data', function (data) {
     cmd = data.toString().trim();
-    commands[cmd]();
-    process.stdout.write('\nprompt > ');
+    let str;
+    if (cmd.slice(0, 4) === 'echo') {
+      str = cmd.slice(5);
+      cmd = 'echo';
+    }
+
+    commands[cmd](str);
 
 });
+
+// echo hello world // => hello world
+// 'echo' => cmd; 'hello world' => str
+// 'echo hello world' => slice(0, 4) => echo
+// str = slice(5);
--- commands.js
+++ commands.js
@@ -1,11 +1,32 @@
+const fs = require('fs');
+
 module.exports = {
-    pwd: function() {
-        process.stdout.write(`${process.cwd()}`);
-      },
+  date: function() {
+    var d = Date(Date.now());
+      process.stdout.write(d);
+      process.stdout.write('\nprompt > ');
+  },
 
-    date: function() {
-      var d = Date(Date.now());
-        process.stdout.write(d);
-      }
-}
+  echo: function() {
+    let args = Array.from(arguments);
 
+    process.stdout.write(args[0]);
+    process.stdout.write('\nprompt > ');
+  },
+
+  ls: function() {
+    fs.readdir('.', function(err, files) {
+      if (err) throw err;
+      files.forEach(function(file) {
+        process.stdout.write(file.toString() + "\n");
+      })
+      process.stdout.write("prompt > ");
+    });
+  },
+
+  pwd: function() {
+      process.stdout.write(`${process.cwd()}`);
+      process.stdout.write('\nprompt > ');
+    },
+
+};
